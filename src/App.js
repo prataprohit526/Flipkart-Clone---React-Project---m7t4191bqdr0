35f9149b69783abe6152d3b6b3cbb15e6b96ab09
prataprohit526 <prataprohit526@gmail.com>
2023-03-25 19:14:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import "./App.css";
+import React, { useEffect } from "react";
 
-function App() {
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Products from "./Pages/Products/Products";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import Cart from "./Pages/Cart/Cart";
+import supabase from "./supabase";
+import { useDispatch } from "react-redux";
+import { setUser } from "./slices/userSlice";
+import Navbar from "./Component/Navbar/Navbar";
+const App = () => {
+  const dispatch = useDispatch();
+
+  // const getUser = async () => {
+  //   const { data, error } = await supabase.auth.getSession();
+  //   dispatch(setUser(data.session.user));
+  // };
+
+  // useEffect(() => {
+  //   getUser();
+  // }, []);
+
   return (
-    <div className="App">
-      <h1>lfhsjdkfueyiu</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/productdetails/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
